60fa011c964225149b7364ccba1586ea6f33f6bf
edipogb <edipogb@gmail.com>
2021-09-27 17:07:03 -0400

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome=None, idade=33):
         self.idade = idade
         self.nome = nome
@@ -19,5 +21,13 @@ if __name__ == '__main__':
         print(filho.nome)
     marcos.sobrenome = 'Silva'
     del marcos.filhos
+    marcos.olhos = 1
     print(marcos.__dict__)
     print(edipo.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(edipo.olhos)
+    print(marcos.olhos)
+    print(id(Pessoa.olhos), id(marcos.olhos), id(edipo.olhos))
+
+
