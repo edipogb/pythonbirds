677d73b71bb9c1e503a66cfd9dec545fc3a4a9fb
edipogb <edipogb@gmail.com>
2021-09-27 15:55:32 -0400

    Criado o atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=33):
+    def __init__(self, *filhos, nome=None, idade=33):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá{id(self)}'
 
 if __name__ == '__main__':
-    p= Pessoa('Marcos')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Édipo'
-    print(p.nome)
-    print(p.idade)
+    edipo = Pessoa(nome='Édipo')
+    marcos = Pessoa(edipo, nome='Marcos')
+    print(Pessoa.cumprimentar(marcos))
+    print(id(marcos))
+    print(marcos.cumprimentar())
+    print(marcos.nome)
+    print(marcos.idade)
+    for filho in marcos.filhos:
+        print(filho.nome)
